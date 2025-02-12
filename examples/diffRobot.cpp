51da299af0853006a26c0bba69a74623f2c70503
loco-choco <contact@locochoco.dev>
2025-02-12 10:55:43 -0300

    changed library to libserialport

--- examples/diffRobot.cpp
+++ examples/diffRobot.cpp
@@ -1,33 +1,35 @@
 #include "kydas_driver/kydas_driver.h"
 #include "ros/ros.h"
 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "kydas_driver");
-    
-    KydasDriver right_driver{16};
-    KydasDriver left_driver{17};
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "kydas_driver");
 
-    if(right_driver.openComport())
-    {
-      ROS_WARN("can't open comport of right driver, the driver might not be connected or the port with the permissions not set");
-      return 0;
-    }
-    if(left_driver.openComport())
-    {
-      ROS_WARN("can't open comport of left driver, the driver might not be connected or the port with the permissions not set");
-      return 0;
-    }
-    ros::Rate rate{25}; //25 hz is the optimal frequency to read/write to the driver without any information getting lost
-    //Makes both drivers go at 1440 dps
-    right_driver.speed_cmd = -1440.f * M_PI / 180; //assuming they are point outwards, we need to negate the right driver
-    left_driver.speed_cmd = 1440.f * M_PI / 180;
-    while(ros::ok()){
-      right_driver.update();
-      left_driver.update();
-      ros::spinOnce();
-      rate.sleep();
-    }
+  KydasDriver right_driver{"/dev/ttyUSB0"};
+  KydasDriver left_driver{"/dev/ttyUSB1"};
 
+  if (right_driver.openComport()) {
+    ROS_WARN("can't open comport of right driver, the driver might not be "
+             "connected or the port with the permissions not set");
     return 0;
+  }
+  if (left_driver.openComport()) {
+    ROS_WARN("can't open comport of left driver, the driver might not be "
+             "connected or the port with the permissions not set");
+    return 0;
+  }
+  ros::Rate rate{25}; // 25 hz is the optimal frequency to read/write to the
+                      // driver without any information getting lost
+  // Makes both drivers go at 1440 dps
+  right_driver.speed_cmd =
+      -1440.f * M_PI / 180; // assuming they are point outwards, we need to
+                            // negate the right driver
+  left_driver.speed_cmd = 1440.f * M_PI / 180;
+  while (ros::ok()) {
+    right_driver.update();
+    left_driver.update();
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  return 0;
 }
--- src/kydas_driver.cpp
+++ src/kydas_driver.cpp
@@ -1,84 +1,95 @@
 #include "kydas_driver/kydas_driver.h"
 
-KydasDriver::KydasDriver(int port, int bdrate, float timeout_time,
-    double speed_filter_weight, double position_filter_weight,
-    int speed_filter_size, int position_filter_size): 
- m_positionInBuf{0}, m_bufSize{0}, m_currentHeaderBeingRead{0}, m_bufferMaxSize{BUFFER_SIZE},
- m_mode{"8N1"},
- isConnected{false},
- m_cport_nr{port}, m_bdrate{bdrate}, m_timeoutTime{timeout_time},
- m_speed_filter_size{speed_filter_size}, m_position_filter_size{position_filter_size},
- m_speed_filter_weight{speed_filter_weight}, m_position_filter_weight{position_filter_weight},
- m_speeds{}, m_positions{}
-{
-  //Creating buffer
+KydasDriver::KydasDriver(const char *port, int bdrate, float timeout_time,
+                         double speed_filter_weight,
+                         double position_filter_weight, int speed_filter_size,
+                         int position_filter_size)
+    : m_positionInBuf{0}, m_bufSize{0}, m_currentHeaderBeingRead{0},
+      m_bufferMaxSize{BUFFER_SIZE}, isConnected{false}, m_bdrate{bdrate},
+      m_timeoutTime{timeout_time}, m_speed_filter_size{speed_filter_size},
+      m_position_filter_size{position_filter_size},
+      m_speed_filter_weight{speed_filter_weight},
+      m_position_filter_weight{position_filter_weight}, m_speeds{},
+      m_positions{} {
+  // Creating buffer
   m_buf = new unsigned char[m_bufferMaxSize];
+  // Retrieving FD of the serial port
+  sp_get_port_by_name(port, &m_cport);
 }
 
-KydasDriver::~KydasDriver(){
+KydasDriver::~KydasDriver() {
   delete[] m_buf;
+  // Close and Free port
+  sp_close(m_cport);
+  sp_free_port(m_cport);
 }
 
-int KydasDriver::openComport(){  
-  return RS232_OpenComport(m_cport_nr, m_bdrate, m_mode.c_str(), 0);
+int KydasDriver::openComport() {
+  if (sp_open(m_cport, SP_MODE_READ_WRITE) < 0)
+    return -1;
+  sp_set_baudrate(m_cport, m_bdrate);
+  // Setting 8N1 mode
+  sp_set_bits(m_cport, 8);
+  sp_set_parity(m_cport, SP_PARITY_NONE);
+  sp_set_stopbits(m_cport, 1);
+  sp_set_flowcontrol(m_cport, SP_FLOWCONTROL_NONE);
 }
 
-void KydasDriver::readSerial(){
-  if(m_currentHeaderBeingRead == 0){ //Nao temos mensagem pendente. ler normalmente
-    m_bufSize = RS232_PollComport(m_cport_nr, m_buf, m_bufferMaxSize - 1);
+void KydasDriver::readSerial() {
+  if (m_currentHeaderBeingRead ==
+      0) { // Nao temos mensagem pendente. ler normalmente
+    m_bufSize = sp_nonblocking_read(m_cport, m_buf, m_bufferMaxSize);
     m_positionInBuf = 0;
-  }
-  else{ //Temos que ler a possivel parte faltante da mensagem
-    //1 - remover o que ja lido (os i-nesimos primeiros bytes) e movimentar o que esta para frente para o inicio
-    //2 - ler a partir de do fim dos que nao foram lidos
-    //3 - voltar i = 0, e m_bufSize = tamanho total (nao lidos + recem lidos)
-    //EXEMPLO: X - lido, + - nao lidos, % - recem lido, - - vazio
-    //m_buf = XXXXX++ (i = 5)
-    //1 - m_buf = -----++ -> m_buf = ++
-    //2 - m_buf = ++%%%
+  } else { // Temos que ler a possivel parte faltante da mensagem
+    // 1 - remover o que ja lido (os i-nesimos primeiros bytes) e movimentar o
+    // que esta para frente para o inicio 2 - ler a partir de do fim dos que nao
+    // foram lidos 3 - voltar i = 0, e m_bufSize = tamanho total (nao lidos +
+    // recem lidos) EXEMPLO: X - lido, + - nao lidos, % - recem lido, - - vazio
+    // m_buf = XXXXX++ (i = 5)
+    // 1 - m_buf = -----++ -> m_buf = ++
+    // 2 - m_buf = ++%%%
     int amount_of_not_read = m_bufSize - m_positionInBuf;
     memcpy(m_buf, &m_buf[m_positionInBuf], amount_of_not_read);
-    int amount_read = RS232_PollComport(m_cport_nr,&m_buf[m_positionInBuf + 1], m_bufferMaxSize - 1 - amount_of_not_read);
+    int amount_read = sp_nonblocking_read(m_cport, &m_buf[m_positionInBuf + 1],
+                                          m_bufferMaxSize - amount_of_not_read);
     m_positionInBuf = 0;
     m_bufSize = amount_of_not_read + amount_read;
   }
   std::string s = displayMessage(m_buf, m_bufSize);
-  const char* cstr = s.c_str();
+  const char *cstr = s.c_str();
   ROS_DEBUG_NAMED(DEBUGGER_NAME_MESSAGE_RECEIVED, "message = [%s]", cstr);
 }
 
-void KydasDriver::readMessagesOnBuffer(){  
+void KydasDriver::readMessagesOnBuffer() {
   bool receivedMessage = false;
 
-  while(m_positionInBuf < m_bufSize){
-    if(m_buf[m_positionInBuf] == HEARTBEAT_HEADER){
+  while (m_positionInBuf < m_bufSize) {
+    if (m_buf[m_positionInBuf] == HEARTBEAT_HEADER) {
       receivedMessage = true;
 
-      if(m_bufSize < 13){
+      if (m_bufSize < 13) {
         m_currentHeaderBeingRead = HEARTBEAT_HEADER;
         ROS_DEBUG("trying to read heartbeat next loop");
         break;
       }
       m_positionInBuf += readHeartbeatData(m_buf, m_positionInBuf);
       m_currentHeaderBeingRead = 0;
-    }
-    else if(m_buf[m_positionInBuf] == QUERY_HEADER){
+    } else if (m_buf[m_positionInBuf] == QUERY_HEADER) {
       receivedMessage = true;
 
-      if(m_bufSize < 6){
+      if (m_bufSize < 6) {
         m_currentHeaderBeingRead = QUERY_HEADER;
         ROS_DEBUG("trying to read query next loop");
         break;
       }
       m_positionInBuf += readQueryData(m_buf, m_positionInBuf);
       m_currentHeaderBeingRead = 0;
-    }
-    else{
+    } else {
       m_positionInBuf++;
     }
   }
-  if(receivedMessage){
-    if(!isConnected){
+  if (receivedMessage) {
+    if (!isConnected) {
       isConnected = true;
       ROS_WARN("driver connected!");
     }
@@ -86,54 +97,53 @@ void KydasDriver::readMessagesOnBuffer(){
   }
 }
 
-void KydasDriver::sendSerial(){
-  if(!isConnected){
+void KydasDriver::sendSerial() {
+  if (!isConnected) {
     return;
-  }  
-  switch (m_currentCommandBeingSent)
-  {
-    case 0:
-      requestQueryData((unsigned char)Query_Data::Speed);
-      break;
-    case 1:
-      requestQueryData((unsigned char)Query_Data::Position);
-      break;
-    case 2:
-      setSpeed(speed_cmd * 180 / M_PI); //command is in rps, but setSpeed works with dps
+  }
+  switch (m_currentCommandBeingSent) {
+  case 0:
+    requestQueryData((unsigned char)Query_Data::Speed);
+    break;
+  case 1:
+    requestQueryData((unsigned char)Query_Data::Position);
+    break;
+  case 2:
+    setSpeed(speed_cmd * 180 /
+             M_PI); // command is in rps, but setSpeed works with dps
   }
   m_currentCommandBeingSent = (m_currentCommandBeingSent + 1) % 3;
 }
 
-void KydasDriver::update()
-{  
-  sendSerial(); //Enviar no serial
-  readSerial(); //Lendo do serial
-  readMessagesOnBuffer(); //Interpretando a mensagem
-  driverReponseCheck(); //Checar se o driver está conectado
+void KydasDriver::update() {
+  sendSerial();           // Enviar no serial
+  readSerial();           // Lendo do serial
+  readMessagesOnBuffer(); // Interpretando a mensagem
+  driverReponseCheck();   // Checar se o driver está conectado
 }
 
-void KydasDriver::driverReponseCheck(){
-  if(isConnected){
-    ros::WallDuration deltaTime = ros::WallTime::now() - m_lastReceivedDataTimeFromDriver;
+void KydasDriver::driverReponseCheck() {
+  if (isConnected) {
+    ros::WallDuration deltaTime =
+        ros::WallTime::now() - m_lastReceivedDataTimeFromDriver;
     double delta = deltaTime.toSec();
-    if(delta > m_timeoutTime){
+    if (delta > m_timeoutTime) {
       isConnected = false;
       ROS_WARN("driver timedout! [%f]", delta);
     }
   }
 }
 
-
-std::string displayMessage(const unsigned char* bytes, int m_bufSize){
+std::string displayMessage(const unsigned char *bytes, int m_bufSize) {
   std::stringstream ss("");
-  
+
   ss << "0x" << std::hex;
-  for (int i = 0; i < m_bufSize; i++) 
-      ss << ' ' << +bytes[i];
-      
+  for (int i = 0; i < m_bufSize; i++)
+    ss << ' ' << +bytes[i];
+
   return ss.str();
 }
 
-std::string displayFaultCode(short faultCode){
+std::string displayFaultCode(short faultCode) {
   return std::bitset<8 * sizeof(faultCode)>(faultCode).to_string();
 }
